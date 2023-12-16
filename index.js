631bcda8d3ad79ae4a6c0fc0a35be23d11ad6590
joydeep53 <149039591+joydeep53@users.noreply.github.com>
2023-12-16 12:10:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ let cross = document.querySelector('.cross')
 let links = document.querySelector('.links')
 menu.addEventListener('click', openMenu)
  function openMenu(){
-    links.style.transform= 'translateY(0%)';
+    links.style.transform= 'translateX(0%)';
+    document.querySelector('body').style.overflow='hidden';
     
  }
  cross.addEventListener('click',closeMenu)
  function closeMenu(){
-    links.style.transform= 'translateY(-110%)';
+    links.style.transform= 'translateX(-100%)';
+    document.querySelector('body').style.overflow='scroll';
 
  }
